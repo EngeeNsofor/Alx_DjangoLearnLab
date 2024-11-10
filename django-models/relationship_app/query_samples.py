219d47ba50516c4d6a75a5cce4eb599ac1e70dfd
Ngozi Nsofor <ngnsofor@yahoo.com>
2024-11-10 07:28:55 +0000

    Updated query_samples.py

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -15,7 +15,7 @@ def get_books_in_library(library_name):
 # Query 3: Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    librarian = library.librarian  # Accesses the OneToOne relation
+    librarian = Librarian.objects.get(library=library) # Accesses the OneToOne relation
     return librarian
 
 # Sample usage of the functions
