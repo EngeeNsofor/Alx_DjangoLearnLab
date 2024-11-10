b825845431c924051f31a9a9f1023b00ff9120b0
Ngozi Nsofor <ngnsofor@yahoo.com>
2024-11-10 08:33:28 +0000

    Updated urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -7,6 +7,9 @@ from django.contrib.auth import views as auth_views  # Import built-in views
 urlpatterns = [
     # URL pattern for the function-based view to list all books
     path('books/', list_books, name='list_books'),
+    path('book/add/', views.add_book, name='add_book'),
+    path('book/<int:pk>/edit/', views.edit_book, name='edit_book'),
+    path('book/<int:pk>/delete/', views.delete_book, name='delete_book'),
 
     # URL pattern for the class-based view to show library details
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
@@ -27,11 +30,4 @@ urlpatterns = [
     path('admin/', views.admin_view, name='admin_view'),
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
-]
-
-
-urlpatterns = [
-    path('book/add/', views.add_book, name='add_book'),
-    path('book/<int:pk>/edit/', views.edit_book, name='edit_book'),
-    path('book/<int:pk>/delete/', views.delete_book, name='delete_book'),
 ]
