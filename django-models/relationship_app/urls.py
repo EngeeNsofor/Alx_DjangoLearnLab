91a2e0aebf71493151f2e70972043a5dfa7f4160
Ngozi Nsofor <ngnsofor@yahoo.com>
2024-11-10 07:54:07 +0000

    Updated urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from .views import list_books, LibraryDetailView  # Import both views for URL routing
+
 
 urlpatterns = [
     # URL pattern for the function-based view to list all books
-    path('books/', views.list_books, name='list_books'),
+    path('books/', list_books, name='list_books'),
 
     # URL pattern for the class-based view to show library details
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
