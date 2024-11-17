46ed4f98b540c806b31edde3c092390f85778e2f
Ngozi Nsofor <ngnsofor@yahoo.com>
2024-11-17 16:26:03 +0000

    updated admin.py

--- advanced_features_and_security/relationship_app/models.py
+++ advanced_features_and_security/relationship_app/models.py
@@ -2,6 +2,7 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.conf import settings
 
 # Custom User Manager
 class CustomUserManager(BaseUserManager):
