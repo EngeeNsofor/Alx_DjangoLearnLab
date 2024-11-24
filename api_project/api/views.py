806e19799c99bb666734a32e8c2589d3007b746c
Ngozi Nsofor <ngnsofor@yahoo.com>
2024-11-24 15:15:58 +0000

    updated views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
@@ -6,5 +5,11 @@ from .serializers import BookSerializer
 # Create your views here.
 
 class BookList(ListAPIView):
+    """
+    API view to list all books in the database.
+    """
     queryset = Book.objects.all()  # Queryset to fetch all Book instances
     serializer_class = BookSerializer  # Serializer to convert data to JSON
+
+
+# generics.ListAPIView
