a1ded6a533ff1c1234607d25c4d401031f20d3aa
Ngozi Nsofor <ngnsofor@yahoo.com>
2024-11-24 16:12:46 +0000

    Updated views.py and urls.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,11 @@
 from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.viewsets import ModelViewSet
 
 # Create your views here.
 
+# generics.ListAPIView
 class BookList(ListAPIView):
     """
     API view to list all books in the database.
@@ -12,4 +14,11 @@ class BookList(ListAPIView):
     serializer_class = BookSerializer  # Serializer to convert data to JSON
 
 
-# generics.ListAPIView
+
+class BookViewSet(ModelViewSet):
+    """
+    A viewset that provides the standard actions
+    for the Book model (CRUD: Create, Read, Update, Delete).
+    """
+    queryset = Book.objects.all()  # Define the data to be managed
+    serializer_class = BookSerializer  # Use the serializer to define data format
