853113340817ba90e889fa8d8e5a700535b0b167
Ngozi Nsofor <ngnsofor@yahoo.com>
2024-12-01 11:48:49 +0000

    Updated views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -6,6 +6,8 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticate
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.filters import SearchFilter, OrderingFilter
+# from django_filters import rest_framework
+# from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
