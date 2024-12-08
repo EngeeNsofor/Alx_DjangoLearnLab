3e03844b8c75e58ed3e306e8598bb090e48cfe54
Ngozi Nsofor <ngnsofor@yahoo.com>
2024-12-08 18:07:53 +0000

    Updates and modifications

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
+from .views import RegisterView, profile_view
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path("login/", LoginView.as_view(template_name="blog/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("profile/", profile_view, name="profile"),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserChangeForm
+from .forms import UserForm, ProfileForm
+from .models import Profile
+
 
 # Create your views here.
 
 def home(request):
     return render(request, 'blog/base.html')
+
+
+class RegisterView(CreateView):
+    template_name = "blog/register.html"
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+
+
+@login_required
+def profile_view(request):
+    if request.method == "POST":
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect("profile")
+    else:
+        form = UserChangeForm(instance=request.user)
+    return render(request, "blog/profile.html", {"form": form})
+
+
+@login_required
+def profile_view(request):
+    user = request.user
+    profile, created = Profile.objects.get_or_create(user=user)
+
+    if request.method == "POST":
+        user_form = UserForm(request.POST, instance=user)
+        profile_form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect("profile")
+    else:
+        user_form = UserForm(instance=user)
+        profile_form = ProfileForm(instance=profile)
+
+    return render(
+        request,
+        "blog/profile.html",
+        {"user_form": user_form, "profile_form": profile_form},
+    )
